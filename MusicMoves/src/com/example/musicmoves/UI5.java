8440b15ea3b7943e92572caedbf861ca8bf132e0
EmanueleFabian <emanuelefabian@hotmail.com>
2014-07-04 17:05:43 +0200

    UI5 commentata! Controllate se va bene!

--- MusicMoves/src/com/example/musicmoves/UI5.java
+++ MusicMoves/src/com/example/musicmoves/UI5.java
@@ -13,29 +13,40 @@ import android.widget.Toast;
 
 public class UI5 extends PreferenceActivity {
 	
+	/*
+	 * Carica le preferences dal file preferences.xml
+	 * Le preferenze cosi fatte vengono direttamente salvate dal
+	 * sistema operativo. Le preference da noi utilizzate sono
+	 * gli assi, il sample rate, l'upsampling, la massima durata di registrazione,
+	 * la durata massima di riproduzione. Per il plus: il ritardo 
+	 * con cui vengono applicati in real time il delay e l'echo 
+	 * e le loro rispettive intensita'.
+	 */
 	@SuppressWarnings("deprecation")
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//      Carica le preferences dal file preferences.xml
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);       
         addPreferencesFromResource(R.xml.preferences);
     } 
       
+	/*
+	 * Il metodo onCreateOptionsMenu() aggiunge il menù.
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-//		Aggiunge menù
 		getMenuInflater().inflate(R.menu.ui5, menu);
 		return true;
 	}
-	
+	/*
+	 *onOptionsItemSelected() permette di riportare le impostazioni
+	 *delle preference ai parametri di default.
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-//		Permette di riportare le impostazioni ai parametri di default
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			SharedPreferences mPreferences= PreferenceManager.getDefaultSharedPreferences (this);
-			
 			SharedPreferences.Editor editor = mPreferences.edit();
 			editor.clear();
 			editor.commit();
